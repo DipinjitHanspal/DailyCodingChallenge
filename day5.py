47f043df82ab77a2f6e360841f05f11837bf7653
Dipinjit Hanspal <hanspal.singhd@gmail.com>
2020-06-02 12:36:30 -0400

    Add problem description for day5

--- day5.py
+++ day5.py
@@ -1,3 +1,10 @@
+"""
+    cons(a, b) constructs a pair, and car(pair) and cdr(pair) 
+    returns the first and last element of that pair. For example, 
+    car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+"""
+
+
 def cons(a, b):
     def pair(f):
         return f(a, b)
@@ -5,10 +12,12 @@ def cons(a, b):
 
 
 def car(pair):
+    # pair accepts function, using lambda for brevity
     return pair(lambda x, y: x)
 
 
 def cdr(pair):
+    # pair accepts function, using lambda for brevity
     return pair(lambda x, y: y)
 
 
